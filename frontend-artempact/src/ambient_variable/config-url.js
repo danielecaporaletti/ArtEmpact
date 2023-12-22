6952acddbefa6a953626c1a60826a27568b75a79
danielecaporaletti <danielecaporaletti1@gmail.com>
2023-12-22 14:06:14 +0100

    some change

--- frontend-artempact/src/ambient_variable/config-url.js
+++ frontend-artempact/src/ambient_variable/config-url.js
@@ -1,3 +1,3 @@
-export const BASE_URL_KEYCLOAK = 'http://34.154.173.122:8080';
-export const BASE_URL_FRONTEND = 'http://34.154.173.122:3000';
-export const BASE_URL_BACKEND = 'http://34.154.173.122:8081';
+export const BASE_URL_KEYCLOAK = 'http://34.16.155.45:8080';
+export const BASE_URL_FRONTEND = 'http://34.16.155.45:3000';
+export const BASE_URL_BACKEND = 'http://34.16.155.45:8081';
