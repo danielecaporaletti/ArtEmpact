2414152c48acf6858e87518dd73c20dbe2d0f070
danielecaporaletti <danielecaporaletti1@gmail.com>
2024-04-09 21:16:56 +0200

    commit

--- backend-artempact/src/main/java/com/artempact/backend/securityConfig/ProjectSecurityConfig.java
+++ backend-artempact/src/main/java/com/artempact/backend/securityConfig/ProjectSecurityConfig.java
@@ -86,7 +86,7 @@ public class ProjectSecurityConfig {
                                 .jwt(jwt ->
                                         jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)
                                 )
-                )
+                );
 
         return http.build();
     }
